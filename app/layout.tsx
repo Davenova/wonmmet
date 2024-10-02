566e0dda3b218924f8a484e069e07e916ebcd744
Amlan Biswas <dudebrothers2@gmail.com>
2024-10-02 20:13:30 +0530

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,29 @@ export default function RootLayout({
       <body>
         <TonConnectUIProvider manifestUrl="https://violet-traditional-rabbit-103.mypinata.cloud/ipfs/QmQJJAdZ2qSwdepvb5evJq7soEBueFenHLX3PoM6tiBffm">
           {children}
+          {/* Footer */}
+          <div className="fixed bottom-0 left-0 right-0 bg-white shadow p-4 flex justify-around">
+            <a href="index.html" className="text-center">
+              <i className="fas fa-home text-gray-500" />
+              <div className="text-sm text-gray-500">Home</div>
+            </a>
+            <a href="wallet.html" className="text-center">
+              <i className="fas fa-trophy text-gray-500" />
+              <div className="text-sm text-gray-500">Wallet</div>
+            </a>
+            <a className="text-center" href="https://make-ton-telegram-mini-app-3-alpha.vercel.app/">
+              <i className="fas fa-user-friends text-gray-500" />
+              <div className="text-sm text-gray-500">Friends</div>
+            </a>
+            <a href="profile.html" className="text-center">
+              <i className="fas fa-cat text-gray-500" />
+              <div className="text-sm text-gray-500">Profile</div>
+            </a>
+            <a href="airdrop.html" className="text-center">
+              <i className="fas fa-gift text-gray-500" />
+              <div className="text-sm text-gray-500">Airdrop</div>
+            </a>
+          </div>
         </TonConnectUIProvider>
       </body>
     </html>
